62262bdaf721f2b16cd03652dba3cd1380c25c92
kozyrovladimir <kozyrovladimir@gmail.com>
2023-06-20 21:34:50 +0300

    HW7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -36,19 +36,24 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // делают студенты
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
     const spanClassName = s.span + (spanProps?.className ? ' ' + spanProps.className : '')
 
     const mappedOptions: any[] = options
-        ? options.map((o) => (
+        ? options.map((o) => {
+          return (
               <label key={name + '-' + o.id} className={s.label}>
                   <input
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
                       // name, checked, value делают студенты
+                      name={name}
+                      checked={Number(value) === o.id}
+                      value={o.id}
 
                       onChange={onChangeCallback}
                       {...restProps}
@@ -61,7 +66,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       {o.value}
                   </span>
               </label>
-          ))
+          )})
         : []
 
     return <div className={s.options}>{mappedOptions}</div>
