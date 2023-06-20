7962eef3b3011d691846e013f7b05563e43f8c61
kozyrovladimir <kozyrovladimir@gmail.com>
2023-06-20 22:49:39 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,14 +4,27 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            state.sort((a, b) => {
+                if (a.name > b.name) {
+                    return 1
+                } else if (a.name < b.name) {
+                    return -1
+                } else {
+                    return 0
+                }
+            })
+
+            if (action.payload === 'down') {
+                return state.reverse()
+            }
 
             return state // need to fix
         }
         case 'check': {
-
+            state = state.filter((u) => u.age >= action.payload)
             return state // need to fix
         }
         default:
